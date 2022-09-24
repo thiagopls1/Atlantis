63975eefe273670a4b2499dfdc411aa356e77440
Thiago Pereira Lins <thiagopls1@hotmail.com>
2022-09-24 15:53:38 -0300

    set application to keep running

--- CardMemoryGame/main.cpp
+++ CardMemoryGame/main.cpp
@@ -2,7 +2,16 @@
 
 int main() {
 
-	al_init();		//Inicialização do Allegro
-	ALLEGRO_DISPLAY* display = al_create_display(1280, 900);		//Criação de "tela"
-	al_rest(10);
+	al_init();
+	ALLEGRO_DISPLAY* display = al_create_display(1280, 720);
+	al_set_window_title(display, "Card Memory Game");
+
+	bool running = true;
+	while(running){
+		al_clear_to_color(al_map_rgb(255, 150, 220));
+		al_flip_display();
+	}
+
+	al_destroy_display(display);
+	return 0;
 }
