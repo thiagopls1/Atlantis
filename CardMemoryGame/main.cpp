4fb9e7bcf283e7285bc2d46222e40f4ac17b3088
Théo Bortoletto <theo.bortolett@gmail.com>
2022-09-24 17:56:35 -0300

    Setup

--- CardMemoryGame/main.cpp
+++ CardMemoryGame/main.cpp
@@ -4,20 +4,25 @@
 int main() {
 //-------------------------------DISPLAY---------------------------------//
     al_init(); //Inicia o Allegro
-    ALLEGRO_DISPLAY* display = al_create_display(1280, 720); //Dimens√µes do Display
+    ALLEGRO_DISPLAY* display = al_create_display(1280, 720); //Dimensıes do Display
+    ALLEGRO_TIMER* timer = al_create_timer(1.0/60);
     al_set_window_title(display, "Memory Game"); //Da o nome do "Display"
     ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue(); //Cria uma fila de eventos
 
  //-------------------------------FILA DE EVENTOS---------------------------------//
+    al_install_mouse();
 
-    al_register_event_source(queue, al_get_display_event_source(display));
+    al_register_event_source(queue, al_get_mouse_event_source());
+    al_register_event_source(queue, al_get_display_event_source(display)); 
+    al_register_event_source(queue, al_get_timer_event_source(timer));
 
     bool running = true;
+    al_start_timer(timer);
     while (running) {
 
         ALLEGRO_EVENT event; //Gera os Eventos
-        al_wait_for_event(queue, &event); //
-
+        al_wait_for_event(queue, &event);
+       
         if (event.type == ALLEGRO_EVENT_TIMER) {
             al_clear_to_color(al_map_rgb(255, 150, 220));
             al_flip_display();
@@ -30,5 +35,6 @@ int main() {
     }
 
     al_destroy_display(display);
+    al_destroy_timer(timer);
     return 0;
 }
