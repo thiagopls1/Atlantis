5632d2b3a6b07c69e891ae6b15d185f6c1e3619d
Théo Bortoletto <theo.bortolett@gmail.com>
2022-09-24 17:48:22 -0300

    Core Setup

--- CardMemoryGame/main.cpp
+++ CardMemoryGame/main.cpp
@@ -2,23 +2,28 @@
 #include <queue>
 
 int main() {
-
-    al_init();
-    ALLEGRO_DISPLAY* display = al_create_display(1280, 720);
-    al_set_window_title(display, "Memory Game");
+//-------------------------------DISPLAY---------------------------------//
+    al_init(); //Inicia o Allegro
+    ALLEGRO_DISPLAY* display = al_create_display(1280, 720); //Dimensões do Display
+    al_set_window_title(display, "Memory Game"); //Da o nome do "Display"
     ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue(); //Cria uma fila de eventos
 
+ //-------------------------------FILA DE EVENTOS---------------------------------//
+
+    al_register_event_source(queue, al_get_display_event_source(display));
 
     bool running = true;
     while (running) {
-        al_clear_to_color(al_map_rgb(255, 150, 220));
-        al_flip_display();
 
-        //Gera os Eventos
-        ALLEGRO_EVENT event;
-        al_wait_for_event(queue, &event);
+        ALLEGRO_EVENT event; //Gera os Eventos
+        al_wait_for_event(queue, &event); //
 
-        if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+        if (event.type == ALLEGRO_EVENT_TIMER) {
+            al_clear_to_color(al_map_rgb(255, 150, 220));
+            al_flip_display();
+        }
+       
+        if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE) //Quando aperta "x" a janela fecha
         {
             running = false;
         }
