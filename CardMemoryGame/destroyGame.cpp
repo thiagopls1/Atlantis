681ca1891aa29df518538c188fac0e35facadc9f
Thiago Pereira Lins <thiagopls1@hotmail.com>
2022-09-29 12:36:50 -0300

    added mouse interaction

--- CardMemoryGame/destroyGame.cpp
+++ CardMemoryGame/destroyGame.cpp
@@ -4,6 +4,7 @@
 
 void destroyGame(ALLEGRO_DISPLAY* display, ALLEGRO_TIMER* timer, ALLEGRO_BITMAP* bitmap) {
 	al_destroy_display(display);
+	al_uninstall_mouse();
 	al_destroy_timer(timer);
 	al_destroy_bitmap(bitmap);
 }
--- CardMemoryGame/main.cpp
+++ CardMemoryGame/main.cpp
@@ -1,10 +1,13 @@
 #include <allegro5\allegro5.h>
 #include <allegro5/allegro_image.h>
+#include <allegro5/allegro_primitives.h>
 #include <queue>
 #include "destroyGame.h"
 #include "registerEventsSource.h"
 
 int main() {
+    int x = 0, y = 0;
+    int r = 0, g = 175, b = 200;
 //-------------------------------DISPLAY---------------------------------//
     al_init(); //Inicia o Allegro
     ALLEGRO_DISPLAY* display = al_create_display(1280, 720); //Dimensões do Display
@@ -19,6 +22,7 @@ int main() {
     al_init_image_addon();
     bitmap = al_load_bitmap("tile.png");
     assert(display != NULL);
+    al_init_primitives_addon();
 
     bool running = true;
     al_start_timer(timer);
@@ -30,9 +34,23 @@ int main() {
         if (event.type == ALLEGRO_EVENT_TIMER) {
             al_clear_to_color(al_map_rgb(0, 150, 220));
             al_draw_bitmap(bitmap, 0, 0, 0);
+            al_draw_rectangle(x, y, x, y, al_map_rgb(r, g, b), 50);
             al_flip_display();
         }
        
+        // Localização do mouse
+        if (event.type == ALLEGRO_EVENT_MOUSE_AXES) {
+            x = event.mouse.x;
+            y = event.mouse.y;
+        }
+
+        // Exemplo de ação ao clicar
+        if (event.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP){
+            r = 210;
+            g = 150;
+            b = 255;
+        }
+
         if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE){
             running = false;
         }
