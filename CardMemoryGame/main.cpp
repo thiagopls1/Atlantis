757279f942abb1ef8833dbda5597b8b37f0bdbf7
Thiago Pereira Lins <thiagopls1@hotmail.com>
2022-09-30 12:50:15 -0300

    created a clickable square

--- CardMemoryGame/main.cpp
+++ CardMemoryGame/main.cpp
@@ -7,7 +7,7 @@
 
 int main() {
     int x = 0, y = 0;
-    int r = 0, g = 175, b = 200;
+    int r = 255, g = 255, b = 255;
 //-------------------------------DISPLAY---------------------------------//
     al_init(); //Inicia o Allegro
     ALLEGRO_DISPLAY* display = al_create_display(1280, 720); //Dimensões do Display
@@ -19,11 +19,16 @@ int main() {
  //-------------------------------FILA DE EVENTOS---------------------------------//
     registerEventsSource(queue, display, timer);
 
+ //-------------------------------INICIALIZAÇÃO DE ADDONS---------------------------------//
+    al_hide_mouse_cursor(display);
     al_init_image_addon();
-    bitmap = al_load_bitmap("tile.png");
-    assert(display != NULL);
     al_init_primitives_addon();
 
+    bitmap = al_load_bitmap("tile.png");
+    assert(display != NULL);
+
+    int displayX = al_get_display_width(display) / 2;
+    int displayY = al_get_display_height(display) / 2;
     bool running = true;
     al_start_timer(timer);
     while (running) {
@@ -34,7 +39,8 @@ int main() {
         if (event.type == ALLEGRO_EVENT_TIMER) {
             al_clear_to_color(al_map_rgb(0, 150, 220));
             al_draw_bitmap(bitmap, 0, 0, 0);
-            al_draw_rectangle(x, y, x, y, al_map_rgb(r, g, b), 50);
+            al_draw_rectangle(displayX - 100, displayY - 100, displayX + 100, displayY + 100, al_map_rgb(r, g, b), 3);
+            al_draw_circle(x, y, 5, al_map_rgb(255, 255, 255), 2);
             al_flip_display();
         }
        
@@ -45,10 +51,19 @@ int main() {
         }
 
         // Exemplo de ação ao clicar
-        if (event.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP){
-            r = 210;
-            g = 150;
-            b = 255;
+        // DOWN: Clicou no botão
+        // UP: Soltou o botão
+        if (event.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN){
+            if (event.mouse.button & 1 && x >= displayX - 100 && x <= displayX + 100 && y >= displayY - 100 && y <= displayY + 100) {
+                r = 0;
+                g = 0;
+                b = 0;
+            }
+            if (event.mouse.button & 2 && x >= displayX - 100 && x <= displayX + 100 && y >= displayY - 100 && y <= displayY + 100) {
+                r = 255;
+                g = 255;
+                b = 255;
+            }
         }
 
         if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE){
