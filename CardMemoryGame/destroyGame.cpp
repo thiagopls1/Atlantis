bdcdd814d14fc3ea9d505413ab33c97eb356c99a
Thiago Pereira Lins <thiagopls1@hotmail.com>
2022-10-07 12:30:20 -0300

    testing text

--- CardMemoryGame/destroyGame.cpp
+++ CardMemoryGame/destroyGame.cpp
@@ -1,10 +1,13 @@
 #include "destroyGame.h"
 #include <allegro5/allegro5.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
 #include <queue>
 
-void destroyGame(ALLEGRO_DISPLAY* display, ALLEGRO_TIMER* timer, ALLEGRO_BITMAP* bitmap) {
+void destroyGame(ALLEGRO_DISPLAY* display, ALLEGRO_TIMER* timer, ALLEGRO_BITMAP* bitmap, ALLEGRO_FONT* font) {
 	al_destroy_display(display);
 	al_uninstall_mouse();
 	al_destroy_timer(timer);
 	al_destroy_bitmap(bitmap);
+	al_destroy_font(font);
 }
--- CardMemoryGame/main.cpp
+++ CardMemoryGame/main.cpp
@@ -1,7 +1,10 @@
 #include <allegro5\allegro5.h>
 #include <allegro5/allegro_image.h>
 #include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
 #include <queue>
+#include <string.h>
 #include "destroyGame.h"
 #include "registerEventsSource.h"
 
@@ -9,10 +12,15 @@ int main() {
     int mouseX = 0, mouseY = 0;
     int r = 255, g = 255, b = 255;
 //-------------------------------DISPLAY---------------------------------//
-    al_init(); //Inicia o Allegro
+    al_init(); //Inicia o Allegro e os seus Addons
+    al_init_image_addon();
+    al_init_ttf_addon();
+    al_init_primitives_addon();
+
     ALLEGRO_DISPLAY* display = al_create_display(1280, 720); //Dimensões do Display
     ALLEGRO_TIMER* timer = al_create_timer(1.0/60);
     ALLEGRO_BITMAP* bitmap;
+    ALLEGRO_FONT* font = al_load_ttf_font("alterebro-pixel.ttf", 40, 0);
     al_set_window_title(display, "Memory Game"); //Da o nome do "Display"
     ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue(); //Cria uma fila de eventos
 
@@ -21,13 +29,11 @@ int main() {
 
  //-------------------------------INICIALIZAÇÃO DE ADDONS---------------------------------//
     al_hide_mouse_cursor(display);
-    al_init_image_addon();
-    al_init_primitives_addon();
-
     bitmap = al_load_bitmap("tile.png");
     assert(display != NULL);
 
     int dialogStep = 0;
+    char dialogText[1000] = {"Olha aqui o texto que legal mto brabo ele hein"};
     bool running = true;
     al_start_timer(timer);
 
@@ -39,7 +45,8 @@ int main() {
         if (event.type == ALLEGRO_EVENT_TIMER) {
             al_clear_to_color(al_map_rgb(0, 150, 220));
             al_draw_bitmap(bitmap, 0, 0, 0);
-            al_draw_rectangle(320, 700, 1250, 500, al_map_rgb(r, g, b), 3);
+            al_draw_rectangle(320, 700, 1250, 600, al_map_rgb(r, g, b), 3);
+            al_draw_text(font, al_map_rgb(255, 255, 255), 330, 600, 0, dialogText);
             al_draw_circle(mouseX, mouseY, 5, al_map_rgb(255, 255, 255), 2);
             al_flip_display();
         }
@@ -54,24 +61,22 @@ int main() {
         // DOWN: Clicou no botão
         // UP: Soltou o botão
         if (event.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN){
-            if (event.mouse.button & 1 && mouseX >= 320 && mouseX <= 1250 && mouseY <= 700 && mouseY >= 500) {
+            if (event.mouse.button & 1 && mouseX >= 320 && mouseX <= 1250 && mouseY <= 700 && mouseY >= 600) {
                 switch (dialogStep) {
                 case 0:
-                    r = 0;
-                    g = 0;
-                    b = 0;
+                    strcpy_s(dialogText, "Mais texto aqui que legal :D");
                     dialogStep++;
                 break;
                 case 1:
-                    r = 100;
-                    g = 255;
-                    b = 100;
+                    strcpy_s(dialogText, "Mais texto ainda aqui que legal!");
                     dialogStep++;
                 break;
                 case 2:
-                    r = 255;
-                    g = 125;
-                    b = 255;
+                    strcpy_s(dialogText, "Aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa '-'");
+                    dialogStep++;
+                break;
+                case 3:
+                    strcpy_s(dialogText, "Hello World.");
                     dialogStep = 0;
                 break;
                 }
@@ -84,6 +89,6 @@ int main() {
         }
     }
 
-    destroyGame(display, timer, bitmap);
+    destroyGame(display, timer, bitmap, font);
     return 0;
 }
